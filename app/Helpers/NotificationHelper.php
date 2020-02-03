b66739f57830abbe05ca42dfb24ea0e791283476
Matt Lantz <mattlantz@gmail.com>
2020-02-02 23:40:06 -0500

    Because its nice to specify who you send to

--- app/Helpers/NotificationHelper.php
+++ app/Helpers/NotificationHelper.php
@@ -23,11 +23,38 @@ if (!function_exists('app_notify')) {
     }
 }
 
+if (!function_exists('app_notify_user')) {
+    function app_notify_user($user, $message, $isImportant = false)
+    {
+        $notification = new InAppNotification($message);
+
+        if ($isImportant) {
+            $notification->isImportant();
+        }
+
+        $user->notify($notification);
+    }
+}
+
 if (!function_exists('email_notify')) {
     function email_notify($subject, $message)
     {
         $user = auth()->user();
 
+        if ($user->allow_email_based_notifications) {
+            Notification::route('mail', $user->email)
+                ->notify(new StandardEmail(
+                    $user->name,
+                    $subject,
+                    $message
+                ));
+        }
+    }
+}
+
+if (!function_exists('email_notify_user')) {
+    function email_notify_user($user, $subject, $message)
+    {
         if ($user->allow_email_based_notifications) {
             Notification::route('mail', $user->email)
                 ->notify(new StandardEmail(
