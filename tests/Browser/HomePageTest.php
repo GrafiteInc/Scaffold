31215c6844bfa7856ab0535a5e4130a0b6a22569
Matt Lantz <mattlantz@gmail.com>
2020-02-19 16:09:25 -0500

    Because dusk is awesome

---
+++ tests/Browser/HomePageTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class HomePageTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testHomePage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->assertSee('Login')
+                ->assertSee('Register');
+        });
+    }
+
+    public function testDashboardAccess()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/dashboard')
+                ->assertSee('Dashboard');
+        });
+    }
+}
---
+++ tests/DuskTestCase.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests;
+
+use App\Models\Role;
+use App\Models\User;
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\Permissions;
+use App\Http\Middleware\VerifyCsrfToken;
+use Laravel\Dusk\TestCase as BaseTestCase;
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
+
+abstract class DuskTestCase extends BaseTestCase
+{
+    use CreatesApplication;
+    use RefreshDatabase;
+
+    public $user;
+
+    /**
+     * Prepare for Dusk test execution.
+     *
+     * @beforeClass
+     * @return void
+     */
+    public static function prepare()
+    {
+        static::startChromeDriver();
+    }
+
+    /**
+     * Create the RemoteWebDriver instance.
+     *
+     * @return \Facebook\WebDriver\Remote\RemoteWebDriver
+     */
+    protected function driver()
+    {
+        $options = (new ChromeOptions())->addArguments([
+            '--disable-gpu',
+            '--headless',
+            '--window-size=1920,1080',
+        ]);
+
+        return RemoteWebDriver::create(
+            'http://localhost:9515',
+            DesiredCapabilities::chrome()->setCapability(
+                ChromeOptions::CAPABILITY,
+                $options
+            )
+        );
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutMiddleware([
+            VerifyCsrfToken::class,
+            EnsureEmailIsVerified::class,
+            Admin::class,
+            Permissions::class,
+        ]);
+
+        $role = factory(Role::class)->create([
+            'name' => 'admin',
+            'label' => 'Admin',
+        ]);
+
+        $this->user = factory(User::class)->create();
+        $this->user->roles()->attach($role->id);
+
+        $this->be($this->user);
+    }
+}
