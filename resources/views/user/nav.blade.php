9bd16bd9bc028243978a9690b24a795d003cd8f6
Matt Lantz <mattlantz@gmail.com>
2020-07-29 10:52:23 -0400

    Because navs could be useful in mobile still

--- resources/views/user/nav.blade.php
+++ resources/views/user/nav.blade.php
@@ -1,4 +1,4 @@
-<ul class="nav nav-tabs mb-4 d-none d-sm-none d-md-flex">
+<ul class="nav nav-tabs mb-4 d-none d-mb-block nav-tabs-custom flex-column flex-sm-row">
     <li class="nav-item">
         <a class="{{ route_link_class('user.settings') }}" href="{{ route('user.settings') }}">Settings</a>
     </li>
