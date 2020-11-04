75bc40d11cc0dbd274548c8c1bf4deffe3fe622f
Matt Lantz <mattlantz@gmail.com>
2020-11-04 12:04:58 -0500

    Because of PWA integration

---
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('app-content')
+
+    <div class="container">
+        <div class="card mt-5">
+            <div class="card-body text-center">
+                <h5 class="m-0">You are currently not connected to any networks.</h5>
+            </div>
+        </div>
+    </div>
+
+@endsection
