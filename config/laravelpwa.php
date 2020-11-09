0f7281f543df6854b85ef865a5b0160922561adc
Matt Lantz <mattlantz@gmail.com>
2020-11-09 06:14:06 -0500

    Better naming

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'name' => 'LaravelPWA',
+    'name' => env('APP_NAME', 'Scaffold'),
     'manifest' => [
-        'name' => env('APP_NAME', 'My PWA App'),
-        'short_name' => 'PWA',
+        'name' => env('APP_NAME', 'Scaffold'),
+        'short_name' => 'Scaffold',
         'start_url' => '/',
         'background_color' => '#ffffff',
         'theme_color' => '#000000',
