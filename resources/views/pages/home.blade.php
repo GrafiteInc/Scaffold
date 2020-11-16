306dd2c3fcb2700818c3c874961f2ce90a12e891
Matt Lantz <mattlantz@gmail.com>
2020-11-16 10:21:38 -0500

    because I forgot when starting another project

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,6 +7,15 @@
     <div class="container">
         <div class="row mt-5">
             <div class="col-md-12 text-center">
+                <div class="card">
+                    <div class="card-body">
+                        <span>Make sure you set <code>SESSION_DOMAIN</code> and <code>SANCTUM_STATEFUL_DOMAINS</code> with your local domain for sessions to function correctly.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-12 text-center">
                 <a class="btn btn-primary m-4" href="{{ url('login') }}">Login</a>
                 <a class="btn btn-primary m-4" href="{{ url('register') }}">Register</a>
             </div>
