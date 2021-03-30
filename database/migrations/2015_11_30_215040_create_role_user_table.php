83100d7de681ee22452d068ef81f8cdd0d2cf13b
Matt Lantz <mattlantz@gmail.com>
2021-03-30 10:30:18 -0400

    because digitalocean doesnt like missing id columns

--- database/migrations/2015_11_30_215040_create_role_user_table.php
+++ database/migrations/2015_11_30_215040_create_role_user_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
@@ -13,6 +14,7 @@ class CreateRoleUserTable extends Migration
     public function up()
     {
         Schema::create('role_user', function (Blueprint $table) {
+            $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('role_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
--- database/migrations/2019_06_06_020202_create_notifications_table.php
+++ database/migrations/2019_06_06_020202_create_notifications_table.php
@@ -14,6 +14,7 @@ class CreateNotificationsTable extends Migration
     public function up()
     {
         Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
             $table->uuid('id')->primary();
             $table->string('type');
             $table->morphs('notifiable');
