373606c54cdac5ee18b7a484c6e585b467948c8e
Matt Lantz <mattlantz@gmail.com>
2021-04-11 22:25:10 -0400

    because of easier splash setup

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -13,6 +13,18 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="card mt-5">
+                    <div class="card-body">
+                        <span>To create PWA icons and splash screens run <code>pwa-asset-generator logo.png ./icons --background "#eee" --portrait-only --type=png</code></span>
+                        <br>
+                        <a target="_blank" href="https://github.com/onderceylan/pwa-asset-generator">https://github.com/onderceylan/pwa-asset-generator</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="row mt-5">
             <div class="col-md-12 text-center">
                 <a class="btn btn-primary m-4" href="{{ url('login') }}">Login</a>
