802cbbd3ea5b6edd7e5068191d8effcd9bb548d2
Matt Lantz <mattlantz@gmail.com>
2023-06-09 12:18:41 -0400

    because of js

--- resources/js/script-components/ajax-forms.js
+++ resources/js/script-components/ajax-forms.js
@@ -1,22 +1,20 @@
 /**
- * This is an example of how we can handle ajax based form submissions.
+ * Handling Ajax form submissions
  */
 window.ajax = (_event) => {
-    let _button = _event.target;
-    let _originalContent = _button.innerHTML;
-
     _event.preventDefault();
 
     let _form = _event.target.closest('form');
+    let _button = _event.target;
 
     if (! _button.hasAttribute('data-formsjs-onclick')) {
         _button = _button.closest('button');
     }
 
     if (_button) {
-        _originalContent = _button.innerHTML;
-        let _processing = '<i class="fas fa-circle-notch fa-spin"></i> ';
-        _button.innerHTML = _processing;
+        let _originalContent = _button.innerHTML;
+        let _processing = '<i class="fas fa-circle-notch fa-spin bmx-fade-in"></i> ';
+        _button.innerHTML = _processing + _originalContent;
     }
 
     if (_form) {
@@ -38,11 +36,12 @@ window.ajax = (_event) => {
             let _event = `${_form.getAttribute('id')}.success`;
             window.app.$events.fire(_event, response.data.data);
 
-            _button.innerHTML = _originalContent;
+            if (_button) {
+                _button.innerHTML = _originalContent;
+            }
         }).catch((error) => {
             // let _event = `${_form.getAttribute('id')}.error`;
             // window.app.$events.fire(_event, error.response.data);
-
             if (error.response && error.response.data) {
                 [error.response.data.errors].forEach((key) => {
                     let _fieldKey = Object.keys(key)[0];
@@ -60,6 +59,10 @@ window.ajax = (_event) => {
 
                     window.Forms_validation();
                 });
+            }
+
+            if (_button) {
+                _button.innerHTML = _originalContent;
             }
         });
     }
