601d8426acfd7cf4589dbeeebd2f1f85554c9f7b
Matt Lantz <mattlantz@gmail.com>
2024-08-27 09:48:20 -0400

    because of some community updates

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,6 +36,7 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+        $this->middleware('auth')->only('logout');
     }
 
     /**
