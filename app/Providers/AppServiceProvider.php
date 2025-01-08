0d998eb6d43f1097281d609f3245932760bb256e
Matt Lantz <mattlantz@gmail.com>
2025-01-08 09:51:37 -0500

    because of system updates and test fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,15 +3,16 @@
 namespace App\Providers;
 
 use App\Models\User;
-use Dedoc\Scramble\Scramble;
-use Illuminate\Pagination\Paginator;
-use Illuminate\Routing\Route;
-use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
-use Illuminate\Validation\Rules\Password;
+use Dedoc\Scramble\Scramble;
 use Laravel\Cashier\Cashier;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
--- tests/Browser/LoginPageTest.php
+++ tests/Browser/LoginPageTest.php
@@ -3,16 +3,19 @@
 namespace Tests\Browser;
 
 use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
 
 class LoginPageTest extends DuskTestCase
 {
     public function testLoginProcess()
     {
         $this->browse(function ($browser) {
-            $browser->visit('/login')
-                ->type('email', $this->user->email)
-                ->type('password', 'secret')
-                ->press('Login')
+            $browser->waitForReload(function (Browser $browser) {
+                $browser->visit('/login')
+                    ->type('email', $this->user->email)
+                    ->type('password', 'secret')
+                    ->press('Login');
+                })
                 ->assertPathIs('/dashboard')
                 ->assertAuthenticated();
         });
