6b347b6e1e20f3ae77a6b3234c2db1c032f36c61
Matt Lantz <mattlantz@gmail.com>
2026-07-27 20:39:25 -0400

    feature: vite compression

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 import purgecss from "@fullhuman/postcss-purgecss";
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig(({ command }) => ({
     plugins: [
@@ -16,6 +17,12 @@ export default defineConfig(({ command }) => ({
                     includeAbsolute: false,
                 },
             },
+        }),
+        viteCompression({
+            algorithm: 'gzip',
+            ext: '.gz',
+            threshold: 1024, // Compresses files larger than 1KB
+            deleteOriginFile: false // Keeps uncompressed versions for compatibility
         }),
     ],
     resolve: {
